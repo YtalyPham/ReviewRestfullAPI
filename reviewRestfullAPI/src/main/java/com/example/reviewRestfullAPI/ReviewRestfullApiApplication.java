2ee90d56d585f83d83e19f604c72989eec1da9ce
YtalyPham <nhuyphamdev@gmail.com>
2025-04-21 09:17:03 +0700

    First commit

---
+++ reviewRestfullAPI/src/main/java/com/example/reviewRestfullAPI/ReviewRestfullApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.reviewRestfullAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewRestfullApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewRestfullApiApplication.class, args);
+	}
+
+}
---
+++ reviewRestfullAPI/src/test/java/com/example/reviewRestfullAPI/ReviewRestfullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reviewRestfullAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewRestfullApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
